d9e57c084f107b6ab5fc02fd85a5f608d1b86329
Elmeri <80104775+elmerij@users.noreply.github.com>
2022-02-09 16:03:33 +0200

    Add files via upload

---
+++ moneygenerator/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace moneygenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(65, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ON";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "keyword";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 38);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Randomize Keyword";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(78, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "OFF";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(155, 88);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "moneygenerator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ moneygenerator/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Ensure your DeepAI.Client NuGet package is up to date: https://www.nuget.org/packages/DeepAI.Client
+// Example posting a text URL:
+
+using DeepAI; // Add this line to the top of your file
+using System.IO;
+
+namespace moneygenerator
+{
+    public partial class Form1 : Form
+    {
+        string resultsFile = @"results.txt";
+        string wordlistFile = @"wordlist.txt";
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Interval=1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (File.Exists(resultsFile))
+            {
+                if (File.Exists(wordlistFile))
+                {
+                    if (checkBox1.Checked)
+                    {
+                        var lines = File.ReadAllLines(wordlistFile);
+                        var r = new Random();
+                        var randomLineNumber = r.Next(0, lines.Length - 1);
+                        var line = lines[randomLineNumber];
+                        DeepAI_API api = new DeepAI_API(apiKey: "f324a1e7-c056-4613-9e27-9cfb0ccad096");
+
+                        StandardApiResponse resp = api.callStandardApi("text2img", new
+                        {
+                            text = line
+                        });
+                        File.AppendAllText(resultsFile,
+                               DateTime.Now.ToString() + " Created NFT with the keyword: " + line + " Link: " + api.objectAsJsonString(resp) + Environment.NewLine);
+                    }
+                    DeepAI_API api2 = new DeepAI_API(apiKey: "f324a1e7-c056-4613-9e27-9cfb0ccad096");
+
+                    StandardApiResponse resp2 = api2.callStandardApi("text2img", new
+                    {
+                        text = textBox2.Text
+                    });
+                    File.AppendAllText(resultsFile,
+                           DateTime.Now.ToString() + " Created NFT with the keyword: " + textBox2.Text + " Link: " + api2.objectAsJsonString(resp2) + Environment.NewLine);
+                }
+                
+            }
+            else
+            {
+                MessageBox.Show("results.txt or wordlist.txt not found");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(resultsFile))
+            {
+                if (File.Exists(wordlistFile))
+                {
+                    
+                }
+
+            }
+            else
+            {
+                File.Create("results.txt");
+                File.Create("wordlist.txt");
+                MessageBox.Show("created results.txt and wordlist.txt, please restart moneygenerator", "moneygenerator");
+                Application.Exit();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
